8b94792fd13b261abcd4625d402360d63331192b
regineheberlein <regineheberlein@users.noreply.github.com>
2023-08-01 19:12:59 -0400

    Update get-manifest-from-MMSID.sh
    cosmetic fix

--- get-manifest-from-MMSID.sh
+++ get-manifest-from-MMSID.sh
@@ -9,6 +9,6 @@ while IFS="," read -r column1 column2 column3 column4
   manifest=$(curl 'https://figgy.princeton.edu/graphql' \
     -H 'content-type: application/json' \
     --data-raw $'{"operationName":"GetResourcesByOrangelightId","variables":{"id":'\"$column1\"'},"query":"query GetResourcesByOrangelightId($id: String\u0021) {\n  resourcesByOrangelightId(id: $id) {\n    id\n    label\n    url\n    embed {\n      type\n      content\n      status\n      __typename\n    }\n    notice {\n      heading\n      acceptLabel\n      textHtml\n      __typename\n    }\n    ... on ScannedResource {\n      manifestUrl\n      __typename\n    }\n    ... on ScannedMap {\n      manifestUrl\n      __typename\n    }\n    ... on Coin {\n      manifestUrl\n      __typename\n    }\n    __typename\n  }\n}"}' | jq .data.resourcesByOrangelightId | jq -r '.[] | .manifestUrl')
-  echo "$column1,$column2, $column3, $column4, $manifest" >> $output_file
-done < <(cut -d ',' -f1,2,3,4 $input_file | tail -n +2) # | head -n 5
+  echo "$column1, $column2, $column3, $column4, $manifest" >> $output_file
+done < <(cut -d ',' -f1,2,3,4 $input_file | tail -n +2)
 
